05130bdf9fbabb1a611081f2fed63fc117a9c5df
PalomoRodriguezHectorLenin <hpalomor2000@alumno.ipn.mx>
2022-04-03 17:52:51 -0500

    JsP3ifelse

--- js/Problema3.js
+++ js/Problema3.js
@@ -3,4 +3,14 @@ function Resultado(){
     console.log('Las horas trabajadas son = ' + horasTrabajo);
     var pagaPorHora = parseInt(document.getElementById('p3-input2').value);
     console.log('El pago es = ' + pagaPorHora);
+
+    if(horasTrabajo<=40){
+        alert(horasTrabajo * pagaPorHora);      
+    }else{
+        if(horasTrabajo>40 && horasTrabajo<48){
+            alert(horasTrabajo * pagaPorHora * 2);
+        }else{
+            alert(horasTrabajo * pagaPorHora * 3);
+        }
+    }
 }
