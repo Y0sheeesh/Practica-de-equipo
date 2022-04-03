8bb08ba0d65ba77e04ada40c225f4ab9049b0460
PalomoRodriguezHectorLenin <hpalomor2000@alumno.ipn.mx>
2022-04-03 17:13:32 -0500

    JsP3Variables

--- js/Problema3.js
+++ js/Problema3.js
@@ -1,3 +1,6 @@
 function Resultado(){
-    
+    var horasTrabajo = parseInt(document.getElementById('p3-input').value);
+    console.log('Las horas trabajadas son = ' + horasTrabajo);
+    var pagaPorHora = parseInt(document.getElementById('p3-input2').value);
+    console.log('El pago es = ' + pagaPorHora);
 }
